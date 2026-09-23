02165ba205d59de3a76b19e6819e1ac5f8f94b83
farahelnashef <farah.elnashef2013@gmail.com>
2026-09-23 20:19:49 +0300

    add data

---
+++ src/constants/data.ts
@@ -0,0 +1,102 @@
+export const data = [
+    {
+        tasktitle:"do homework",
+        date:"24/9/2026",
+        time:"16:30",
+        statos:"done",
+        location:"math club"
+    },
+     {
+        tasktitle:"sport",
+        date:"29/9/2026",
+        time:"18:00",
+        statos:"done",
+        location:"basketball club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:"english club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"15:00",
+        statos:"done",
+        location:" club"
+    },
+    {
+        tasktitle:"basketball",
+        date:"25/12/2026",
+        time:"19:00",
+        statos:"done",
+        location:"arabic club"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/10/2026",
+        time:"20:00",
+        statos:"done",
+        location:"hebro"
+    },
+    {
+        tasktitle:"study english",
+        date:"14/1/2027",
+        time:"15:00",
+        statos:"done",
+        location:"art club"
+    },
+    
+    
+]
